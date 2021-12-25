9f9262a14cb83fa19c6c6c9d3ca045a81db7647c
cristal <cristal@atguigu.com>
2021-12-25 23:58:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Description
+ * @Author Crystal
+ * @Date 2021-12-25 23:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
