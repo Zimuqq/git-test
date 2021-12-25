ef7bfb4703eb1f04f69f5822ed26edeb9c666757
cristal <cristal@atguigu.com>
2021-12-26 01:30:00 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master hello");
         System.out.println("hello git4");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
